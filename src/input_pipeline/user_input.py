260fcdbce476058d74391bd97e67bda463920d6a
Mohammed <mail.mohammedadam@gmail.com>
2025-12-29 14:13:02 +0530

    pdf file extraction through pymupdf

--- src/input_pipeline/user_input.py
+++ src/input_pipeline/user_input.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import tempfile
 from src.utils import check_file_extension, check_file_size
 from pathlib import Path 
+import shutil
+from src.utils import route_filetype
 
  
 class FileInput():
@@ -31,11 +33,15 @@ class FileInput():
                         logging.info('file size is valid, return file path')
                         
                         temp_dir = tempfile.mkdtemp()
-                        with open(self.file_path, "w", encoding='utf-8') as f:
-                            f.write(temp_dir)
-                            logging.info('file saved in temp dir')
-                            
-                            return self.file_path   
+                        
+                        filename = os.path.basename(self.file_path)
+                        temp_file_path = os.path.join(temp_dir, filename)
+                        
+                        # copying the file
+                        shutil.copy2(self.file_path, temp_file_path)
+
+                        return temp_dir, temp_file_path
+                           
                         
                     else:
                         print(f"invalid file size, filesize supported till 100Mb")   
@@ -49,13 +55,33 @@ class FileInput():
         except Exception as e:
             logging.info('invalid file')
             raise CustomException(e, sys)
+    
+    def extract_text(self, file_path):
+        """Pass the file path to their specific file extractors,
+        and return the extracted content"""
         
+        try:
+            
+            if not os.path.exists(file_path):
+                raise FileNotFoundError(f"The specified path was not found: {file_path}")
+            
+            file_content = route_filetype(file_path)
+            print(file_content)
+            logging.info('extracted content from the file')
+            return file_content
         
+        except Exception as e:
+            logging.info('failed to extract content from the file')
+            raise CustomException(e, sys)
+        
+    
+    
 if __name__ == '__main__':
     
-    check_filepath = FileInput('data/test.txt')  
-    file_path = check_filepath.return_valid_file()
-                    
-                    
+    check_filepath = FileInput('data/mohammedAdamResume.pdf')  
+    temp_dir, file_path = check_filepath.return_valid_file()
+    file_content = check_filepath.extract_text(file_path)
+    
+    shutil.rmtree(temp_dir) 
             
             
