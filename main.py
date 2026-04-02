e3d762c2ef2949e9f9953fb5025ce7e8a0f24642
Mohammed <mail.mohammedadam@gmail.com>
2026-04-02 14:03:31 +0530

    Add login redirecting

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
+        "http://localhost:3000",
         "http://localhost:5500",
         "https://resume-parser-r9dg.onrender.com",
     ],
